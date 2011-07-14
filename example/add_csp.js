45ae0c4e4c4bb7b2a1a43c0902c36a2bc789d67d
Mark Percival <m@mdp.im>
2011-07-14 08:20:51 -0700

    Adding csp header example

---
+++ example/add_csp.js
@@ -0,0 +1,21 @@
+var Mf = require('../lib/index');
+var url = require('url');
+var fs = require('fs');
+
+
+var addCSP = function(urlRegex) {
+  return function(req, res, next){
+    if (req.fullUrl.match(urlRegex)) {
+      var writeHead = res.writeHead;
+      res.writeHead = function(){
+        var headers = arguments[arguments.length-1];
+        var statusCode = arguments[0];
+        headers['x-content-security-policy'] = "allow 'self'";
+        writeHead.call(res, statusCode, headers);
+      };
+    }
+    next();
+  };
+};
+
+Mf.createProxy(addCSP(/google.com/)).listen(8088).listenHTTPS(8089);
